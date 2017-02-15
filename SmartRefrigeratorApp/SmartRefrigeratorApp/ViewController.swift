f589dd05a539268a821aa46c9550087105162e43
何柏融 <peter810725@hotmail.com.tw>
2017-02-15 20:24:10 +0800

    finished keyin page, but still need to figure out how to reload food data on the table view

--- SmartRefrigeratorApp/SmartRefrigeratorApp/ViewController.swift
+++ SmartRefrigeratorApp/SmartRefrigeratorApp/ViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var foodListTableView: UITableView!
-    var foodListContent = ["apple", "meat"]
+    var foodListContent = [[String]]()
+    
+    @IBOutlet weak var foodName: UITextField!
+    @IBOutlet weak var foodQuantity: UITextField!
+    @IBOutlet weak var foodExpireDate: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,10 +35,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
     
         let Cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "foodCell")
-        Cell.textLabel?.text = foodListContent[indexPath.row]
+        Cell.textLabel?.text = foodListContent[indexPath.row][1]
         return Cell
     }
 
+    @IBAction func saveAndNext(_ sender: Any) {
+        var foodInfo = [String]()
+        foodInfo.append(foodName.text!)
+        foodInfo.append(foodQuantity.text!)
+        foodInfo.append(foodExpireDate.text!)
+        foodListContent.append(foodInfo)
+    }
+    
+    @IBAction func saveAndList(_ sender: Any) {
+        var foodInfo = [String]()
+        foodInfo.append(foodName.text!)
+        foodInfo.append(foodQuantity.text!)
+        foodInfo.append(foodExpireDate.text!)
+        foodListContent.append(foodInfo)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
